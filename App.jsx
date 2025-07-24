10590c283c40f43aaa5af031ee6174276e89dcbd
Tumpa Biswas <144771268+Tumpa433@users.noreply.github.com>
2025-07-24 12:14:52 +0530

    main

---
+++ App.jsx
@@ -0,0 +1,24 @@
+import About from "./components/About/About"
+import Contect from "./components/Contact/Contect"
+import Home from "./components/Home/Home"
+import Nav from "./components/Nav/Nav"
+import Projects from "./components/Projects/Projects"
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Nav/>
+      <Home/>
+      <About/>
+      <Projects/>
+      <Contect/>
+
+    </>
+  )
+}
+
+export default App
