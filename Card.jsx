0e62b0c308bbb15ee586f0cb924a6d27394d69b4
Tumpa Biswas <144771268+Tumpa433@users.noreply.github.com>
2025-07-24 12:10:41 +0530

    card

---
+++ Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Card.css"
+
+const Card = ({title, image}) => {
+  return (
+    <div className='card'>
+      <h2>{title}</h2>
+      <div className="hovercard">
+        <img src={image} alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Card
